eed80eb01f3da0e3bdf9ce8341d9a812881113e8
Álvaro Condorí <condorialvaro95@gmail.com>
2025-05-08 22:01:05 -0300

    Building the Sign-Up Form UI & Creating the Profile Photo Selector Component

--- frontend/src/pages/Auth/Registrarse.jsx
+++ frontend/src/pages/Auth/Registrarse.jsx
@@ -1,7 +1,101 @@
-import React from "react";
+import React, {useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
+import AuthLayout from "../../components/Layouts/AuthLayout";
+import Input from "../../components/Inputs/Input";
+import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector";
+import {validateEmail} from "../../utils/helper";
 
 const Registrarse = () => {
-  return <div>Registrarse</div>;
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  // Handle Sign Up Form Submit
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    let profileImageUrl = "";
+
+    if (!fullName) {
+      setError("Por favor, ingresá tu nombre");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Por favor, ingresá una dirección de correo electrónico válida");
+      return;
+    }
+
+    if (!password) {
+      setError("Por favor, ingresá la contraseña");
+      return;
+    }
+
+    setError("");
+
+    // SignUp API Call
+  };
+
+  return (
+    <AuthLayout>
+      <div className='lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center'>
+        <h3 className='text-xl font-semibold text-black'>Crear una Cuenta</h3>
+        <p className='text-xs text-slate-700 mt-[5px] mb-6'>
+          Sumate hoy ingresando tus datos a continuación.
+        </p>
+
+        <form onSubmit={handleSignUp}>
+          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+            <Input
+              value={fullName}
+              onChange={({target}) => setFullName(target.value)}
+              label='Nombre Completo'
+              placeholder='Juan Cito'
+              type='text'
+            />
+
+            <Input
+              value={email}
+              onChange={({target}) => setEmail(target.value)}
+              label='Correo electrónico'
+              placeholder='ejemplo@email.com'
+              type='text'
+            />
+
+            <div className='col-span-2'>
+              <Input
+                value={password}
+                onChange={({target}) => setPassword(target.value)}
+                label='Contraseña'
+                placeholder='Mínimo 8 caracteres'
+                type='password'
+              />
+            </div>
+          </div>
+
+          {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+
+          <button type='submit' className='btn-primary'>
+            REGISTRATE
+          </button>
+
+          <p className='text-[13px] text-slate-800 mt-3'>
+            ¿Ya tenés una cuenta?{" "}
+            <Link className='font-medium text-primary underline' to='/ingresar'>
+              Iniciá sesión
+            </Link>
+          </p>
+        </form>
+      </div>
+    </AuthLayout>
+  );
 };
 
 export default Registrarse;
