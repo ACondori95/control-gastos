51713f74bd2abdd616400bdafbe4c5c6a3d0757d
Álvaro Condorí <condorialvaro95@gmail.com>
2025-05-09 03:03:48 -0300

    Creating Income APIs (Add Income, Get All Income, Delete Income, Download Excel Data)

---
+++ backend/controllers/incomeControllers.js
@@ -0,0 +1,76 @@
+const Ingreso = require("../models/Ingreso");
+const xlsx = require("xlsx");
+
+// Add Income Source
+exports.addIncome = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const {icon, source, amount, date} = req.body;
+
+    // Validation: Check for missing fields
+    if (!source || !amount || !date) {
+      return res
+        .status(400)
+        .json({message: "Todos los campos son obligatorios"});
+    }
+
+    const newIncome = new Ingreso({
+      userId,
+      icon,
+      source,
+      amount,
+      date: new Date(date),
+    });
+
+    await newIncome.save();
+    res.status(200).json(newIncome);
+  } catch (error) {
+    res.status(500).json({message: "Error del servidor"});
+  }
+};
+
+// Get All Income Source
+exports.getAllIncome = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const income = await Ingreso.find({userId}).sort({date: -1});
+    res.json(income);
+  } catch (error) {
+    res.status(500).json({message: "Error del servidor"});
+  }
+};
+
+// Delete Income Source
+exports.deleteIncome = async (req, res) => {
+  try {
+    await Ingreso.findByIdAndDelete(req.params.id);
+    res.json({message: "Ingreso eliminado con éxito"});
+  } catch (error) {
+    res.status(500).json({message: "Error del servidor"});
+  }
+};
+
+// Download Excel
+exports.downloadIncomeExcel = async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const income = await Ingreso.find({userId}).sort({date: -1});
+
+    // Prepare data for excel
+    const data = income.map((item) => ({
+      Fuente: item.source,
+      Importe: item.amount,
+      Fecha: item.date,
+    }));
+
+    const wb = xlsx.utils.book_new();
+    const ws = xlsx.utils.json_to_sheet(data);
+    xlsx.utils.book_append_sheet(wb, ws, "Ingresos");
+    xlsx.writeFile(wb, "ingresos_detalle.xlsx");
+    res.download("ingresos_detalle.xlsx");
+  } catch (error) {
+    res.status(500).json({message: "Error del servidor"});
+  }
+};
