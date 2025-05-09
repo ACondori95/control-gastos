1e2b5d08634e6517dc0bcea54859cc0a2735f31c
Álvaro Condorí <condorialvaro95@gmail.com>
2025-05-09 20:41:10 -0300

    Fetching Dashboard Data via API Call

--- frontend/src/pages/Dashboard/Inicio.jsx
+++ frontend/src/pages/Dashboard/Inicio.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import DashboardLayout from "../../components/Layouts/DashboardLayout";
 import {useUserAuth} from "../../hooks/useUserAuth";
+import {useNavigate} from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import {API_PATHS} from "../../utils/apiPaths";
 
 const Inicio = () => {
   useUserAuth();
+
+  const navigate = useNavigate();
+
+  const [dashboardData, setDashboardData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchDashboardData = async () => {
+    if (loading) return;
+
+    setLoading(true);
+
+    try {
+      const response = await axiosInstance.get(
+        `${API_PATHS.DASHBOARD.GET_DATA}`
+      );
+
+      if (response.data) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.log("Algo salió mal. Por favor, intentá de nuevo.", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDashboardData();
+    return () => {};
+  }, []);
 
   return (
     <DashboardLayout activeMenu='Inicio'>
