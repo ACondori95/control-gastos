759e90f5db002bf11fa9bf6c61078f06f7cd069c
Álvaro Condorí <condorialvaro95@gmail.com>
2025-05-09 20:24:42 -0300

    Creating the Dashboard Page Layout

--- frontend/src/pages/Dashboard/Inicio.jsx
+++ frontend/src/pages/Dashboard/Inicio.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import DashboardLayout from "../../components/Layouts/DashboardLayout";
+import {useUserAuth} from "../../hooks/useUserAuth";
 
 const Inicio = () => {
-  return <div>Inicio</div>;
+  useUserAuth();
+
+  return (
+    <DashboardLayout activeMenu='Inicio'>
+      <div className='my-5 mx-auto'>Inicio</div>
+    </DashboardLayout>
+  );
 };
 
 export default Inicio;
